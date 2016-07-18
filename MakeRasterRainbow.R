f3e48c767b952800d71c7724ab4bd5a0e7923013
Ben Rogers <bjroger@ku.edu>
2016-07-18 09:58:00 -0500

    changed tst variable name to borders, and played with layering a bit.

--- MakeRasterRainbow.R
+++ MakeRasterRainbow.R
@@ -14,22 +14,21 @@ if(!exists("bra2"))bra2 <- readShapePoly(paste0(maploc, "BRA_adm2.shp"))
 
 names(bra2)
 
-
-if(!exists("brafort")) brafort <- fortify(bra2, region = "ID_2")
+## Bit of a problem here. I don't want to create brafort every time, since fortifying brazil takes a while, but I do want to create it every time I change the region
+if(!exists("brafort")) brafort <- fortify(bra2, region = "ID_1")
 brazil <- brafort[seq(1, nrow(brafort), 10),]
 dim(brazil)
 
 range(brazil$long)
 
 str(bra2)
-if(!exists("tbra")) tbra <- fortify(bra2, region = "ID_0")
 
 
 
 ## Box outside brazil?
-tst <- unionSpatialPolygons(bra2, bra2$ID_0)
+if(!exists("borders")) borders <- unionSpatialPolygons(bra2, bra2$ID_0)
 
-bra2simp <- gSimplify(tst, .0284)
+bra2simp <- gSimplify(borders, .0284)
 brenv <- gEnvelope(bra2simp)
 brout <- gDifference(brenv, bra2simp, byid = F)
 ## brout2 <- gIntersection(brenv, bra2simp,  byid = T, drop_lower_td = T)
@@ -38,27 +37,30 @@ broutFort <- fortify(brout)
 brFortSh <- broutFort
 ## brFortSh$order <- 1:nrow(brFortSh)
 
-
+## Make the rainbow!
 rainseq <- seq(min(brFortSh$long)+.1, max(brFortSh$long)-.1, by = .01)
 barseq <- rep(mean(brFortSh$lat), length.out = length(rainseq))
 coloration <- sort(rep(1:7, length.out = length(rainseq)))
-
 rainbowdf <- data.frame(rainseq, "barseq" = barseq, coloration)
+
 
 dev.new()
 pdf(paste0(maindir,"BrazilRB.pdf"))
+##
 u <- ggplot() + geom_tile(data = rainbowdf, aes(x = rainseq, y = barseq, fill = rainbow(7)[coloration]), height = 5 + diff(range(brazil$lat))) + scale_fill_identity()
 ## Filling with alpha allows us to make/control its transparency.
 u <- u + geom_polygon(data = brazil, aes(x = long, y = lat, group = group), color = "white", fill = alpha("white", 0))  + theme(panel.background = element_blank())
-###
+## Layer on part hiding everything that isn't brazil
 u <- u + geom_polygon(data = broutFort, aes(x = long, y = lat, group = group), fill = "white")
 u <- u + theme(axis.title = element_blank(), axis.ticks = element_blank(), axis.text = element_blank())
 ###
+### Add layers to hide additional rainbow atop and below
+topaes <- aes(xmin = min(long), xmax = max(long), ymin = max(lat) - 0.035, ymax = max(lat) + 5 )
+bottomaes <- aes(xmin = min(long), xmax = max(long), ymax = min(lat), ymin = min(lat) - 0.6)
+u <- u + geom_rect(data = broutFort, topaes, fill = "white") + geom_rect(data = broutFort, bottomaes, fill = "white")
+u
 ###
-u + coord_cartesian(ylim = c(min(brFortSh$lat) + 1.85, max(brFortSh$lat) - 1.8))
+## u + coord_map()
 graphics.off()
 
 ## Try something different
-
-
-ggplot(aes(long, lat, group = group), data = tbra2) + geom_polygon() + theme(panel.background = element_rect("grey"))
