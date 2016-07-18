86550db05c0ac8bd5caf0faae74ca5cb9ec888bd
Ben Rogers <bjroger@ku.edu>
2016-07-18 09:19:52 -0500

    so map u works. Need to figure out what to do about borders though.

--- MakeRasterRainbow.R
+++ MakeRasterRainbow.R
@@ -15,25 +15,24 @@ if(!exists("bra2"))bra2 <- readShapePoly(paste0(maploc, "BRA_adm2.shp"))
 names(bra2)
 
 
-reg <- "ID_1"
-if(!exists("brafort")) brafort <- fortify(bra2, region = reg)
+if(!exists("brafort")) brafort <- fortify(bra2, region = "ID_2")
 brazil <- brafort[seq(1, nrow(brafort), 10),]
 dim(brazil)
 
 range(brazil$long)
 
 str(bra2)
-tbra <- fortify(bra2, region = "ID_0")
-tbra <- tbra[seq(1, nrow(tbra), 100),]
-tattempt <- SpatialPointsDataFrame(data.frame(tbra$long, tbra$lat), data = tbra)
+if(!exists("tbra")) tbra <- fortify(bra2, region = "ID_0")
+
 
 
 ## Box outside brazil?
-bra2simp <- gSimplify(bra2, .0284)
+tst <- unionSpatialPolygons(bra2, bra2$ID_0)
+
+bra2simp <- gSimplify(tst, .0284)
 brenv <- gEnvelope(bra2simp)
 brout <- gDifference(brenv, bra2simp, byid = F)
 ## brout2 <- gIntersection(brenv, bra2simp,  byid = T, drop_lower_td = T)
-
 
 broutFort <- fortify(brout)
 brFortSh <- broutFort
@@ -46,15 +45,20 @@ coloration <- sort(rep(1:7, length.out = length(rainseq)))
 
 rainbowdf <- data.frame(rainseq, "barseq" = barseq, coloration)
 
-
-u <- ggplot() + geom_tile(data = rainbowdf, aes(x = rainseq, y = barseq, fill = rainbow(7)[coloration]), height = 3 + diff(range(brazil$lat))) + scale_fill_identity()
-
+dev.new()
+pdf(paste0(maindir,"BrazilRB.pdf"))
+u <- ggplot() + geom_tile(data = rainbowdf, aes(x = rainseq, y = barseq, fill = rainbow(7)[coloration]), height = 5 + diff(range(brazil$lat))) + scale_fill_identity()
 ## Filling with alpha allows us to make/control its transparency.
 u <- u + geom_polygon(data = brazil, aes(x = long, y = lat, group = group), color = "white", fill = alpha("white", 0))  + theme(panel.background = element_blank())
-
-u + geom_polygon(data = broutFort, aes(x = long, y = lat, group = group), fill = "white")  + coord_cartesian(ylim = c(min(brFortSh$lat), max(brFortSh$lat)))
+###
+u <- u + geom_polygon(data = broutFort, aes(x = long, y = lat, group = group), fill = "white")
+u <- u + theme(axis.title = element_blank(), axis.ticks = element_blank(), axis.text = element_blank())
+###
+###
+u + coord_cartesian(ylim = c(min(brFortSh$lat) + 1.85, max(brFortSh$lat) - 1.8))
+graphics.off()
 
 ## Try something different
 
 
-ggplot(aes(long, lat, group = group), data = tbra) + geom_polygon() + theme(panel.background = element_rect("grey"))
+ggplot(aes(long, lat, group = group), data = tbra2) + geom_polygon() + theme(panel.background = element_rect("grey"))
