a4a9fa2df5d7ec0adfcbca791f36168cc58f2a30
Ben Rogers <bjroger@ku.edu>
2016-07-18 10:40:57 -0500

    changed name of borders to borderPoly
    I think something else is called borders in R, and as such the exists
    command kept giving me an error.

--- MakeRasterRainbow.R
+++ MakeRasterRainbow.R
@@ -1,6 +1,6 @@
 ## Create a set of bars equivalent to a rainbow, so I don't have to
 ## keep messing around with all these probabilities.
-rm(list = ls()[!ls() %in% c("bra2", "brafort", "broutFort", "brout")])
+rm(list = ls()[!ls() %in% c("bra2", "brafort", "broutFort", "brout", "borderPoly")])
 library(maptools)
 library(ggplot2)
 library(rgeos)
@@ -10,7 +10,7 @@ maploc <- "/Users/bjr/GitHub/MapProj/Shp/BRA_adm_shp/"
 maindir <- "/Users/bjr/Dropbox/R_Projects/MapSomething/RBmaps/"
 set.seed(123456)
 
-if(!exists("bra2"))bra2 <- readShapePoly(paste0(maploc, "BRA_adm2.shp"))
+if(!exists("bra2")) bra2 <- readShapePoly(paste0(maploc, "BRA_adm2.shp"))
 
 names(bra2)
 
@@ -29,20 +29,19 @@ str(bra2)
 ## together all the polygons with a certain id. Since ID_0 represents
 ## being a part of Brazil, this gets all of the polygons inside the
 ## mainland. Wish there was a way to make it faster though...
-if(!exists("borders")) borders <- unionSpatialPolygons(bra2, bra2$ID_0)
+if(!exists("borderPoly")) borderPoly <- unionSpatialPolygons(bra2, bra2$ID_0)
 
-bra2simp <- gSimplify(borders, .0284)
+bra2simp <- gSimplify(borderPoly, tol = .029)
 brenv <- gEnvelope(bra2simp)
 brout <- gDifference(brenv, bra2simp, byid = F)
 ## brout2 <- gIntersection(brenv, bra2simp,  byid = T, drop_lower_td = T)
-
 broutFort <- fortify(brout)
-brFortSh <- broutFort
+## brFortSh <- broutFort
 ## brFortSh$order <- 1:nrow(brFortSh)
 
 ## Make the rainbow!
-rainseq <- seq(min(brFortSh$long)+.1, max(brFortSh$long)-.1, by = .01)
-barseq <- rep(mean(brFortSh$lat), length.out = length(rainseq))
+rainseq <- seq(min(broutFort$long)+.1, max(broutFort$long)-.1, by = .01)
+barseq <- rep(mean(broutFort$lat), length.out = length(rainseq))
 coloration <- sort(rep(1:7, length.out = length(rainseq)))
 rainbowdf <- data.frame(rainseq, "barseq" = barseq, coloration)
 
@@ -52,7 +51,7 @@ pdf(paste0(maindir,"BrazilRB.pdf"))
 ##
 u <- ggplot() + geom_tile(data = rainbowdf, aes(x = rainseq, y = barseq, fill = rainbow(7)[coloration]), height = 5 + diff(range(brazil$lat))) + scale_fill_identity()
 ## Filling with alpha allows us to make/control its transparency.
-u <- u + geom_polygon(data = brazil, aes(x = long, y = lat, group = group), color = "white", fill = alpha("white", 0))  + theme(panel.background = element_blank())
+u <- u + geom_polygon(data = brazil, aes(x = long, y = lat, group = group), color = "white", fill = alpha("white", 0))  + theme(panel.background = element_blank()) + coord_quickmap()
 ## Layer on part hiding everything that isn't brazil
 u <- u + geom_polygon(data = broutFort, aes(x = long, y = lat, group = group), fill = "white")
 u <- u + theme(axis.title = element_blank(), axis.ticks = element_blank(), axis.text = element_blank())
@@ -63,6 +62,5 @@ bottomaes <- aes(xmin = min(long), xmax = max(long), ymax = min(lat), ymin = min
 u <- u + geom_rect(data = broutFort, topaes, fill = "white") + geom_rect(data = broutFort, bottomaes, fill = "white")
 u
 ###
-## u + coord_map()
 graphics.off()
 
